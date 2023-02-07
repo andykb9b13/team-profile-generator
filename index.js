0b49427609d590b81eee8980ee8a615c4d959bf5
Andy Kleindienst <andy.kleindienst@gmail.com>
2023-02-07 09:42:20 -0500

    set the path for the generated html and css files to dist dir

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern')
 
+
 function initiateProgram() {
     inquirer
         .prompt({
@@ -13,9 +14,10 @@ function initiateProgram() {
         })
         .then((response) => {
             if (response.init) {
-                askQuestions()
+                askQuestions();
             } else {
                 console.log("Thanks! Goodbye!")
+                writeFiles();
                 return
             }
         }
@@ -51,16 +53,16 @@ function askQuestions() {
             console.log(response)
             switch (response.role) {
                 case "manager":
-                    makeManager(response.name, response.role, response.id, response.email)
+                    makeManager(response.name, response.role, response.id, response.email);
                     break
                 case "intern":
-                    makeIntern(response.name, response.role, response.id, response.email)
+                    makeIntern(response.name, response.role, response.id, response.email);
                     break
                 case 'engineer':
-                    makeEngineer(response.name, response.role, response.id, response.email)
+                    makeEngineer(response.name, response.role, response.id, response.email);
                     break
                 case 'end':
-                    console.log("GoodBye!")
+                    console.log("GoodBye!");
                     return
             }
         })
@@ -75,14 +77,16 @@ function makeManager(name, role, id, email) {
             name: "officeNumber"
         })
         .then((response) => {
-            newManager.officeNumber = response.officeNumber
-            console.log(newManager)
-            initiateProgram()
+            newManager.officeNumber = response.officeNumber;
+            console.log(newManager);
+            initiateProgram();
+            return newManager;
         })
 }
 
+
 function makeEngineer(name, role, id, email) {
-    const newEngineer = new Engineer(name, role, id, email);
+    let newEngineer = new Engineer(name, role, id, email);
     inquirer
         .prompt({
             type: "text",
@@ -90,11 +94,14 @@ function makeEngineer(name, role, id, email) {
             name: "github"
         })
         .then((response) => {
-            newEngineer.github = `https://github.com/${response.github}`
+            newEngineer.github = `https://github.com/${response.github}`;
+            initiateProgram();
             console.log(newEngineer)
-            initiateProgram()
+            return newEngineer;
         })
 }
+
+
 
 function makeIntern(name, role, id, email) {
     const newIntern = new Intern(name, role, id, email);
@@ -106,12 +113,105 @@ function makeIntern(name, role, id, email) {
         })
         .then((response) => {
             newIntern.school = response.school;
-            console.log(newIntern)
-            initiateProgram()
+            console.log(newIntern);
+            initiateProgram();
         })
 }
 
 initiateProgram()
 
+function writeFiles() {
+    fs.writeFile("./dist/index.html", `<!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800&display=swap"
+            rel="stylesheet">
+        <link rel="stylesheet" href="./style.css">
+        <title>Document</title>
+    </head>
+    
+    <body>
+        <h1>This is a test HTML file</h1>
+        <div id="cardContainer">
+            <div class="teamMember card">
+                <h2>Intern</h2>
+                <ul>
+                    <li id="managerName">Andy</li>
+                    <li id="employeeId">1249023</li>
+                    <li id="employeeEmail">andy.kleindienst@gmail.com</li>
+                    <li id="officeNumber">919-949-1840</li>
+                </ul>
+            </div>
+            <div class="teamMember card">
+                <h2>Intern</h2>
+                <ul>
+                    <li id="managerName">Andy</li>
+                    <li id="employeeId">1249023</li>
+                    <li id="employeeEmail">andy.kleindienst@gmail.com</li>
+                    <li id="officeNumber">919-949-1840</li>
+                </ul>
+            </div>
+            <div class="teamMember card">
+                <h2>Engineer</h2>
+                <ul>
+                    <li id="managerName">Andy</li>
+                    <li id="employeeId">1249023</li>
+                    <li id="employeeEmail">andy.kleindienst@gmail.com</li>
+                    <li id="officeNumber">919-949-1840</li>
+                </ul>
+            </div>
+            <div class="teamMember card">
+                <h2>Engineer</h2>
+                <ul>
+                    <li id="managerName">Andy</li>
+                    <li id="employeeId">1249023</li>
+                    <li id="employeeEmail">andy.kleindienst@gmail.com</li>
+                    <li id="officeNumber">919-949-1840</li>
+                </ul>
+            </div>
+            <div class="teamMember card">
+                <h2>Engineer</h2>
+                <ul>
+                    <li id="managerName">Andy</li>
+                    <li id="employeeId">1249023</li>
+                    <li id="employeeEmail">andy.kleindienst@gmail.com</li>
+                    <li id="officeNumber">919-949-1840</li>
+                </ul>
+            </div>
+        </div>
+    
+    </body>
+    
+    </html>`, (err) => (err) ? console.log("whoops, something went wrong") : console.log("wrote index.html file"));
 
-
+    fs.writeFile('./dist/style.css', `body {
+        font-family: 'Poppins', sans-serif;
+    }
+    
+    h1 {
+        text-align: center;
+    }
+    
+    h2 {
+        text-align: center;
+    }
+    
+    #cardContainer {
+        display: flex;
+        flex-direction: inline-block;
+        flex-wrap: wrap;
+    }
+    
+    .teamMember {
+        background-color: paleturquoise;
+        border: 1px solid black;
+        box-shadow: 1px 1px 2px 1px black;
+        margin: 5px;
+    }`, (err) => (err) ? console.log("whoops, couldn't write the css file") : console.log('wrote the css file"'));
+}
